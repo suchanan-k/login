099b1f5977ace91beb123ef616fe517882edbea3
Suchanan Kwanmuang <suchanan.kw@gmail.com>
2020-10-07 02:46:25 +0700

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
+
 function App() {
+  
+  
+  const [login, setLogin] = useState({
+    username:'',
+    password:''
+  })
+  const [isPasswordShown, setIsPasswordShown] = useState('')
+
+
+  const handleChange= (e)=>{
+    const { name, value } =e.target;
+    setLogin(prevValue =>{
+      return {
+        ...prevValue,
+        [name]:value
+      }
+    })
+  }
+
+
+  
+  const togglePassword = () =>{
+    setIsPasswordShown({isPasswordShown: !isPasswordShown})
+
+  }
+
+  const handleClick = (e)=>{
+    
+    e.preventDefault();
+    setLogin({
+      username:'',
+      password:''
+    })
+    if (login.password === 'P@$$W0RD'){
+      alert("ยินดีต้อนรับ " +  login.username)
+        }
+        else {
+          alert('รหัสผ่านไม่ถูกต้อง')
+        }
+      }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" >
+    
+      <h1 >LOGIN</h1>
+      <form >
+        <input 
+        required
+        placeholder="username"
+        name="username"
+        value={login.username}
+        onChange={handleChange}
+        type="text"
+        />
+        <div>
+        <input 
+        required
+        placeholder="password"
+        name="password"
+        value={login.password}
+        onChange={handleChange}
+        type={(isPasswordShown) ? "text": "password"}
+        />
+        <i 
+        className="fa fa-eye" 
+        onClick={togglePassword}
+        />
+        </div>
+
+        <button 
+        type="submit"
+        onClick={handleClick} 
+        >LOGIN
+        </button>
+      </form>
+      </div>
   );
 }
 
 export default App;
+
